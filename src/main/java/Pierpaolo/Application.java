2cab9ef0950988309badf1b9c81b3765204cad60
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2023-12-09 18:14:40 +0100

    es 3

--- src/main/java/Pierpaolo/Application.java
+++ src/main/java/Pierpaolo/Application.java
@@ -16,7 +16,7 @@ public class Application {
 
         Libro libro1 = new Libro("45678-45962","Sapiens",2018,345,"Juval Noah Harari","Storico");
         Rivista rivista1 = new Rivista("7490-4516","Al Volante",2023,330, Periodicità.MENSILE);
-        Libro libro2 = new Libro("41678-4592","Homo-Deus",2020,399,"Juval Noah Harari","Storico");
+        Libro libro2 = new Libro("41678-4592","Homo-Deus",2022,399,"Juval Noah Harari","Storico");
         Rivista rivista2 = new Rivista("490-4516","Topolino",2023,210, Periodicità.SETTIMANALE);
         Libro libro3 = new Libro("5778-4562","Il nuovo Java",2015,857,"Claudio De Sio Cesari","Informatica");
         Rivista rivista3 = new Rivista("890-416","Nature",2022,610, Periodicità.ANNUALE);
@@ -46,6 +46,13 @@ public class Application {
         String sapiensISBN = "45678-45962";
         List<Metadati> ricercaISBN = archivio.stream().filter(prodotto->prodotto.getISBN().equals(sapiensISBN)).toList();
         ricercaISBN.forEach(ricerca-> System.out.println("Ricerca da ISBN: " + ricerca.getTitolo()));
+        System.out.println();
+        System.out.println("-------------------------------------------------   es 3   --------------------------------------------------------------");
+        System.out.println("--------------------------------     ricerca elemento dato un anno   --------------------------------------------------");
+        int ricercaANNO = 2022;
+        List<Metadati> ricercaByAnno = archivio.stream().filter(prodotto->prodotto.getAnnoPubblicazione() == ricercaANNO).toList();
+        System.out.print("Libri pubblicati nell'anno " + ricercaANNO + " :");
+        ricercaByAnno.forEach(ricerca-> System.out.print(ricerca.getTitolo() + ", "));
 
 
 
